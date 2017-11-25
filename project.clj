bc6a30927e4f165429676704399f6a284ad3d354
Shogo Ohta <athos0220@gmail.com>
2017-11-25 22:44:11 +0900

    Update URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kitchen-async "0.1.0-SNAPSHOT"
   :description "Yet another async library for ClojureScript aiming for seamless use of Promises and core.async channels with ease"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/athos/kitchen-async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
