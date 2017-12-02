f7a4a696a4fd30d1cd3d87bfbbb68f43aaa74167
OHTA Shogo <athos0220@gmail.com>
2017-12-02 23:39:52 +0900

    Add tests for other fns and macros

--- test/kitchen_async/promise_test.cljs
+++ test/kitchen_async/promise_test.cljs
@@ -104,3 +104,115 @@
                 (fn [e]
                   (is (= msg (ex-message e)))
                   (done))))))
+
+(deftest all-test
+  (async done
+    (let [ch (a/chan)
+          p (p/all [(p/resolve 42) ch])]
+      (a/put! ch 43)
+      (p/then p (fn [x]
+                  (= [42 43] x)
+                  (done))))))
+
+(deftest race-test
+  (async done
+    (p/then (p/race [(p/resolve 42) (a/chan)])
+            (fn [x]
+              (is (= 42 x))
+              (done)))))
+
+(deftest timeout-test
+  (async done
+    (let [t (js/Date.)]
+      (p/then (p/timeout 100 42)
+              (fn [x]
+                (is (>= (- (js/Date.) t) 100))
+                (is 42 x)
+                (done))))))
+
+(deftest let-simple-test
+  (async done
+    (let [p (p/let [x (p/resolve 41)]
+              (+ x 1))]
+      (is (instance? Promise p))
+      (p/then p (fn [x]
+                  (is (= 42 x))
+                  (done))))))
+
+(deftest let-multiple-test
+  (async done
+    (let [p (p/let [x (p/resolve 40)
+                    y (p/promise [resolve]
+                        (js/setTimeout #(resolve (inc x)) 0))]
+              (inc y))]
+      (p/then p (fn [x]
+                  (is (= 42 x))
+                  (done))))))
+
+(deftest let-polymorphic-test
+  (async done
+    (let [ch (a/chan)
+          p (p/let [x 40
+                    y ch]
+              (+ x y))]
+      (a/put! ch 2)
+      (p/then p (fn [x]
+                  (is (= 42 x))
+                  (done))))))
+
+(deftest plet-test
+  (async done
+    (let [ch (a/chan)
+          p (p/plet [x (p/resolve 21)
+                     y ch]
+              (+ x y))]
+      (a/put! ch 21)
+      (p/then p (fn [x]
+                  (is (= 42 x))
+                  (done))))))
+
+(deftest loop-test
+  (async done
+    (let [ch (a/chan)
+          p (p/loop [v ch
+                     sum 0]
+              (if (> v 0)
+                (p/recur ch (+ sum v))
+                sum))]
+      (a/put! ch 20)
+      (a/put! ch 22)
+      (a/put! ch 0)
+      (p/then p (fn [x]
+                  (is (= x 42))
+                  (done))))))
+
+(deftest while-test
+  (async done
+    (let [ch (a/chan)
+          a (atom 0)
+          p (p/while (<= @a 30)
+              (p/let [v ch]
+                (swap! a + v)))]
+      (a/put! ch 20)
+      (a/put! ch 22)
+      (p/then p (fn [_]
+                  (is (= 42 @a))
+                  (done))))))
+
+(deftest ->-test
+  (async done
+    (p/then (p/-> (p/timeout 0 39)
+                  inc
+                  (+ 2))
+            (fn [x]
+              (is (= 42 x))
+              (done)))))
+
+(deftest ->>-test
+  (async done
+    (p/then (p/->> (p/resolve 41)
+                   (p/timeout 0)
+                   inc)
+            (fn [x]
+              (is (= 42 x))
+              (done)))))
