773993b227acf17f27a50436f5fe08716fdd5a29
OHTA Shogo <athos0220@gmail.com>
2018-01-06 00:10:41 +0900

    Remove unnecessary files

--- test/kitchen_async/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns kitchen-async.core-test
-  (:require [kitchen-async.core :as k]
-            [clojure.test :refer [deftest is are testing]]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
